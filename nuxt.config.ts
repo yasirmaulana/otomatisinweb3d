0d4563b215e430a01d8a190f392b245e104053e7
yasirmaulana <yasir.maulana@gmail.com>
2026-05-20 19:42:45 +0700

    update vercel analytics plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@vercel/analytics'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     supabaseUrl: process.env.SUPABASE_URL || '',
